6256f51f1513344dc29ea2cb7ce2c04c25ce08f0
SAMARPIT AGARWAL <agarwalsamarpit@gmail.com>
2024-03-10 00:08:27 +0530

    signup logic with the password encryption is created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 const app = express();
 const prepareandstartserver = () => {
+    
+    app.use(bodyParser.json());
+     app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
     app.listen(PORT,()=>{
         console.log(`server started on port : ${PORT}`);
     })
